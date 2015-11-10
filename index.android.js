f41fcf50c1331ca71a40c31199cbbd967c2fc555
Denny <abalone0204@gmail.com>
2015-11-11 01:32:41 +0800

    feat(index.android.js): ListView
    Implement ListView and familar with  layout view

--- index.android.js
+++ index.android.js
@@ -3,7 +3,7 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
-
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 var MOCKED_MOVIES_DATA = [
   {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
 ];
@@ -14,38 +14,99 @@ var {
   StyleSheet,
   Text,
   View,
-  Image
+  Image,
+  ListView
 } = React;
 
 var AwesomeProject = React.createClass({
+  getInitialState: function () {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2 
+      }),
+      loaded: false
+    };
+  },
+  componentDidMount: function () {
+    this.fetchData();
+  },
+  fetchData: function () {
+     fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true
+        });
+      })
+      .done();
+  },
   render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    };
+    // var movie = this.state.movies[0];
+    // return this.renderMovie(movie);
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+      />
+      );
+  },
+  renderLoadingView: function() {
+     return (
+        <View style={styles.container}>
+          <Text>
+            Loading movies...
+          </Text>
+        </View>
+      );
+  },
+  renderMovie: function(movie) {
     return (
       <View style={styles.container}>
-        <Text>{movie.title}</Text>
-        <Text>{movie.year}</Text>
-         <Image source={{uri: movie.posters.thumbnail}} />
+        <Image 
+          style={styles.thumbnail} 
+          source={{uri: movie.posters.thumbnail}} 
+          />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
       </View>
-    );
+      );
   }
 });
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
-  welcome: {
+  rightContainer: {
+    flex: 1,
+  },
+  thumbnail: {
+    width: 53, 
+    height: 81
+  },
+   title: {
     fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    margin: 10,
   },
-  instructions: {
+  year: {
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: "#F5FCFF"
+  }
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
---
+++ notes/01-hello-world.md
@@ -0,0 +1,29 @@
+- react native 用 style object 的方式來呈現，會用到許多 flex 的個概念
+
+- [flex guide](https://css-tricks.com/snippets/css/a-guide-to-flexbox/)
+
+# ListView
+
+顯示部份資料，這樣效能會比較好，
+ListView 會只顯示現在螢幕出現的部分，
+離開的話就會將他移掉。
+
+## API 介紹
+
+```js
+<ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+      />
+```
+
+- dataSource: 資料來源，必須從 ListView 底下呼叫 DataSource 來初始化，同時要給他一個判定 row 有在變動的 function
+
+```js
+new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2 
+      })
+```
+
+- renderRow: 從 dataSource 中每次拿出來顯示要用的 function
